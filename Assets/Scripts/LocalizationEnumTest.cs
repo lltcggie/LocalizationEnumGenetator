d710fe09aa72e8cd7a824d5848d3daa179f19596
lltcggie <7032792+lltcggie@users.noreply.github.com>
2025-05-24 04:23:50 +0900

    ver 1.0.0

---
+++ Assets/Scripts/LocalizationEnumTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class LocalizationEnumTest : MonoBehaviour
+{
+    void Start()
+    {
+        var Text1Key0 = LangTextKey.LangTextKeyUtil.GetLocalizedText(LangTextKey.Text1.Key0);
+        var Text1Key1 = LangTextKey.LangTextKeyUtil.GetLocalizedText(LangTextKey.Text1.Key1);
+        var Text2Key0 = LangTextKey.LangTextKeyUtil.GetLocalizedText(LangTextKey.Text2.Key0);
+        var Text2Key1 = LangTextKey.LangTextKeyUtil.GetLocalizedText(LangTextKey.Text2.Key1);
+        Debug.Log(Text1Key0);
+        Debug.Log(Text1Key1);
+        Debug.Log(Text2Key0);
+        Debug.Log(Text2Key1);
+    }
+}
